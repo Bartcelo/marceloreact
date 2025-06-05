162c935d7e3e22de24b1fad66074c141dbb30e0a
marcelo <bart.celo@hotmail.com>
2025-06-05 08:03:06 -0300

    atualizando o secrets

--- src/componentes/sobre.jsx
+++ src/componentes/sobre.jsx
@@ -10,7 +10,7 @@ function Sobre() {
                 </div>
                 <div className=" w-auto sm:w-1/2 flex items-center">
 
-                    <p className="text-center">Sou Marcelo Gonzaga, desenvolvedor Full Stack com mais de 3 anos de experiência, apaixonado por transformar ideias em soluções digitais eficientes. Minha jornada começou como professor de informática em 2022, onde descobri minha vocação para a tecnologia. Em outubro de 2024, dei um novo passo ao ingressar no curso de Análise e Desenvolvimento de Sistemas, onde hoje aprimoro meus conhecimentos no segundo semestre.
+                    <p className="text-center">Meu nome é Marcelo Gonzaga, desenvolvedor Full Stack com mais de 3 anos de experiência, apaixonado por transformar ideias em soluções digitais eficientes. Minha jornada começou como professor de informática em 2022, onde descobri minha vocação para a tecnologia. Em outubro de 2024, dei um novo passo ao ingressar no curso de Análise e Desenvolvimento de Sistemas, onde hoje aprimoro meus conhecimentos no segundo semestre.
 
                         Domino tecnologias como HTML, CSS, JavaScript, Dart, Flutter, Laravel, PHP, Tailwind e C#, além de experiência em APIs REST. Um dos meus projetos recentes inclui o desenvolvimento do site romafla.com.br, onde pude aplicar minha expertise em criação de interfaces funcionais e robustas.
 
